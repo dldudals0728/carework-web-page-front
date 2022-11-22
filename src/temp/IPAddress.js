0bd258e5b3bcbab81ef4e63a7f6975547d3abd20
Young Min <dldudals0728@gachon.ac.kr>
2022-11-23 02:21:06 +0900

    AWS 연동 / gh-pages 수정

--- src/temp/IPAddress.js
+++ src/temp/IPAddress.js
@@ -1,5 +1,5 @@
 // AWS Server
-// export const IP_ADDRESS = "http://15.165.230.213:8080";
+export const IP_ADDRESS = "http://15.165.230.213:8080";
 
 // local
-export const IP_ADDRESS = "http://172.30.1.35:8080";
+// export const IP_ADDRESS = "http://172.30.1.35:8080";
